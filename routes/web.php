1d6500fa4970b8ec969b51cbad99c65ce01b6d31
Abrar Faiyaz <abrarfaiyaz8@gmail.com>
2024-03-19 22:15:26 +0600

    First commit

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ use App\Http\Controllers\ShopController;
 
 Route::get('/',[AppController::class,'index'])->name('app.index');
 Route::get('/shop',[ShopController::class,'index'])->name('shop.index');
+Route::get('/shop/{slug}',[ShopController::class,'productDetails'])->name('shop.product.details');
 
 Auth::routes();
 
