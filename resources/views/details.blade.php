e2c7c3d84868bdee33ec802eac8c26d7e6934a00
Abrar Faiyaz <abrarfaiyaz8@gmail.com>
2024-03-21 22:32:18 +0600

    Second commit

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.base')
 @push('styles')
     <link id="color-link" rel="stylesheet" type="text/css" href="{{ asset('assets/css/demo2.css') }}">
+    <style>
+        .product-box .product-details h5 {
+            width: 100%;
+        }
+    </style>
 @endpush
 @section('content')
 <section class="breadcrumb-section section-b-space" style="padding-top:20px;padding-bottom:20px;">
@@ -201,18 +206,13 @@
                                         <i class="fa fa-bookmark fz-16 me-2"></i>
                                         <span>Wishlist</span>
                                     </a>
-                                    <a href="javascript:void(0)"                                            
+                                    <a href="javascript:void(0)"  onclick="event.preventDefault(),document.getElementById('addtocart').submit();"                                          
                                         id="cartEffect" class="btn btn-solid hover-solid btn-animation">
                                         <i class="fa fa-shopping-cart"></i>
                                         <span>Add To Cart</span>
-                                        <form id="addtocart" method="post"
-                                            action="http://localhost:8000/cart/store">
-                                            <input type="hidden" name="_token"
-                                                value="MkRqEzTGuoSx6LqJUm0OAKxSgNUYt26wTT7RMUZY"> <input
-                                                type="hidden" name="id" value="1">
-                                            <input type="hidden" name="name"
-                                                value="Autem Repudiandae Accusantium Blanditiis">
-                                            <input type="hidden" name="price" value="13">
+                                        <form id="addtocart" method="post" action="{{ route('cart.store') }}">
+                                            @csrf
+                                            <input type="hidden" name="id" value="{{ $product->id }}">>
                                             <input type="hidden" name="quantity" id="qty" value="1">
                                         </form>
                                     </a>
@@ -872,9 +872,9 @@
                                     </div>
                                     <h3 class="theme-color">
                                         @if ($rproduct->sale_price)
-                                            {{ $rproduct->sale_price }}
+                                            ${{ $rproduct->sale_price }}
                                         @else
-                                            {{ $rproduct->regular_price }}
+                                            ${{ $rproduct->regular_price }}
                                         @endif
                                     </h3>
                                     <button onclick="location.href = 'cart.html';" class="btn listing-content">Add
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AppController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ShopController;
+use App\Http\Controllers\CartController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,9 @@ use App\Http\Controllers\ShopController;
 Route::get('/',[AppController::class,'index'])->name('app.index');
 Route::get('/shop',[ShopController::class,'index'])->name('shop.index');
 Route::get('/shop/{slug}',[ShopController::class,'productDetails'])->name('shop.product.details');
+Route::get('/cart',[CartController::class,'index'])->name('cart.index');
+Route::post('/cart/store',[CartController::class,'addToCart'])->name('cart.store');
+Route::put('/cart/update',[CartController::class,'updateCart'])->name('cart.update');
 
 Auth::routes();
 
