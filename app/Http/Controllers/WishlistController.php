79c7a6a5d3ae072ebe397ab7eca1daf0c4fd3d56
Abrar Faiyaz <abrarfaiyaz8@gmail.com>
2024-04-03 15:59:41 +0600

    7th commit

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -7,8 +7,24 @@ use Cart;
 
 class WishlistController extends Controller
 {
+    public function getWishlistProducts(){
+        $items = Cart::instance('wishlist')->content();
+        return view('wishlist',['items' => $items]);
+    }
+
     public function addProductToWishlist(Request $request){
         Cart::instance('wishlist')->add($request->id, $request->name,1,$request->price)->associate('App\Models\Product');
         return response()->json(['status'=>200,'message'=>'Success ! Item successfully added to your wishlist.']);
+    }
+
+    public function removeProductFromWishlist(Request $request){
+        $rowId = $request->rowId;
+        Cart::instance('wishlist')->remove($rowId);
+        return redirect()->route('wishlist.list');
+    }
+
+    public function clearWishlist(){
+        Cart::instance('wishlist')->destroy();
+        return redirect()->route('wishlist.list');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -30,7 +30,10 @@ Route::put('/cart/update',[CartController::class,'updateCart'])->name('cart.upda
 Route::delete('/cart/remove',[CartController::class,'removeItem'])->name('cart.remove');
 Route::delete('/cart/clear',[CartController::class,'clearCart'])->name('cart.clear');
 
+Route::get('/wishlist',[WishlistController::class,'getWishlistProducts'])->name('wishlist.list');
 Route::post('/wishlist/add',[WishlistController::class,'addProductToWishlist'])->name('wishlist.store');
+Route::delete('/wishlist/remove',[WishlistController::class,'removeProductFromWishlist'])->name('wishlist.remove');
+Route::delete('/wishlist/clear',[WishlistController::class,'clearWishlist'])->name('wishlist.clear');
 
 Auth::routes();
 
